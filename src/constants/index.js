84fc55bc8c863ede20aa04d149e452e1556a1d07
Shubham Rawat <144698152+shubhmrwt01@users.noreply.github.com>
2026-07-16 20:38:39 +0530

    Update URL for Resume in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -333,7 +333,7 @@ const socialImgs = [
   {
     name: "Resume",
     imgPath: "/images/document.svg",
-    url: "https://drive.google.com/file/d/1fRfJUtkEwB9ha_nrgmZBqawZgpzsg5Z8/view",
+    url: "https://drive.google.com/file/d/1SWB1bz77StrIj6sdheAhE1I6Y2z76L-F/view?usp=sharing",
   },
 ];
 
